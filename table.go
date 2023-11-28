0217b1112fb995dbdddc6fd6b929af07b66594a7
ostrbor <ostrbor@gmail.com>
2023-11-28 16:32:06 -0300

    table struct that is parsed from model struct

---
+++ table.go
@@ -0,0 +1,98 @@
+package qry
+
+import (
+	"errors"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+const (
+	tableTag     = "table"
+	columnTag    = "column"
+	generatedTag = "generated"
+)
+
+type table struct {
+	name    string
+	columns []column
+}
+
+type column struct {
+	name           string
+	generated      bool // indicates if the column is generated in database (serial or identity)
+	structFieldPtr interface{}
+}
+
+func names(cols []column) []string {
+	res := make([]string, 0, len(cols))
+	for _, c := range cols {
+		res = append(res, c.name)
+	}
+	return res
+}
+
+func pointers(cols []column) []interface{} {
+	res := make([]interface{}, 0, len(cols))
+	for _, c := range cols {
+		res = append(res, c.structFieldPtr)
+	}
+	return res
+}
+
+func parse(structOrStructPtr reflect.Value) (t table, err error) {
+	s := reflect.Indirect(structOrStructPtr)
+	if s.Kind() != reflect.Struct {
+		return t, errKind(s.Kind(), reflect.Struct)
+	}
+
+	var tableName string
+	var cols []column
+	var generatedCount, tableCount int
+	for i := 0; i < s.NumField(); i++ {
+		f := s.Type().Field(i)
+		colName, generated := parseColumn(f.Tag)
+		if colName == "" {
+			continue
+		}
+		cols = append(cols, column{
+			name:           colName,
+			generated:      generated,
+			structFieldPtr: s.Field(i).Addr().Interface(),
+		})
+		if generated {
+			generatedCount++
+		}
+		tab := f.Tag.Get(tableTag)
+		if tab != "" {
+			tableName = tab
+			tableCount++
+		}
+	}
+
+	if len(cols) == 0 {
+		return t, errors.New("missing 'column' in tags")
+	}
+	if tableCount == 0 {
+		return t, errors.New("missing 'table' in tags")
+	} else if tableCount > 1 {
+		return t, fmt.Errorf("must be one 'table', got %d", tableCount)
+	}
+	if generatedCount > 1 {
+		return t, fmt.Errorf("must be one 'generated', got %d", generatedCount)
+	}
+
+	t.name = tableName
+	t.columns = cols
+	return t, nil
+}
+
+func parseColumn(t reflect.StructTag) (name string, gen bool) {
+	c := t.Get(columnTag)
+	if c == "" {
+		return "", false
+	}
+	name = strings.Split(c, ",")[0]
+	gen = strings.Contains(c, generatedTag)
+	return name, gen
+}
