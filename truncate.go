f40abaa91a9d71ccd7e6e86e7a0caf40c5ff79e4
ostrbor <ostrbor@gmail.com>
2023-12-04 13:26:58 -0300

    switch to anonymous func

--- truncate.go
+++ truncate.go
@@ -4,28 +4,29 @@ import (
 	"context"
 )
 
+// Migrations is a constant that represents the name of the migrations table.
 var Migrations = "migrations"
 
+// TruncateAll is a function that truncates all tables in the PostgreSQL database,
+// excluding the migrations table. It uses an anonymous function (DO block) to execute
+// the SQL command. This function is intended to be used in a testing environment where
+// you need to clean up the database before each test.
 func TruncateAll(db Querier) {
 	sql := `
-	-- Truncate all tables in the public schema
-	CREATE OR REPLACE FUNCTION truncate_all() RETURNS void LANGUAGE plpgsql AS
-	$$
-	BEGIN
-		EXECUTE
-		(SELECT 'TRUNCATE TABLE '
-		-- Escape table names using %I to handle potential keywords or spaces
-		|| string_agg(format('%I', tablename), ', ')
-		-- CASCADE ensures truncating tables with references to the specified tables
-		-- RESTART IDENTITY restarts sequences owned by truncated tables
-		|| ' CASCADE RESTART IDENTITY'
-		FROM pg_tables
-		WHERE schemaname = 'public' AND tablename != '` + Migrations + `'
-		);
-	END
-	$$;
-	-- Call the truncate_all function
-	SELECT truncate_all();`
+ DO $$
+ BEGIN
+  EXECUTE
+  (SELECT 'TRUNCATE TABLE '
+  -- Escape table names using %I to handle potential keywords or spaces
+  || string_agg(format('%I', tablename), ', ')
+  -- CASCADE ensures truncating tables with references to the specified tables
+  -- RESTART IDENTITY restarts sequences owned by truncated tables
+  || ' CASCADE RESTART IDENTITY'
+  FROM pg_tables
+  WHERE schemaname = 'public' AND tablename != '` + Migrations + `'
+  );
+ END
+ $$;`
 	_, err := db.Exec(context.Background(), sql)
 	if err != nil {
 		panic(err)
